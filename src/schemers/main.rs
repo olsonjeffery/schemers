0baa31dd7b6b526e31118684abc928a9921882a3
Jeff Olson <olson.jeffery@gmail.com>
2014-05-16 22:31:51 -0500

    test/impl for tokenize() + some flush-out of main()

--- src/schemers/main.rs
+++ src/schemers/main.rs
@@ -4,26 +4,48 @@
 // at the top-level of this repository.
 // This file may not be copied, modified, or distributed
 // except according to those terms.
-use std::os::args;
+#![crate_id="http://olsonjeffery.github.io#schemers:0.0.1"]
+#![desc = "Simple Scheme Interpreter"]
+#![license = "3-Clause BSD"]
 
 // this is starting out as a straight port of Peter Norvig's
 // lis.py (the first iteration) to Rust
 
+#[cfg(not(test))]
 fn main() {
-    let _args = args();
-    println!("{}", ~"");
+    use std::os::args;
+    let args: Vec<~str> = args().move_iter().skip(1).collect();
+    if args.len() == 0 {
+        fail!("no program provided");
+    }
+    let program: ~str = args.move_iter()
+        .fold(~"", |memo, arg| memo + arg + " ").trim().to_owned();
+    println!("{}", tokenize(program));
 }
 
+// parser impl
 fn pad_input(input: ~str) -> ~str {
     input.replace("(", " ( ").replace(")", " ) ")
 }
 
+fn tokenize(input: ~str) -> Vec<~str> {
+    pad_input(input).split_str(" ")
+        .filter(|i| *i != "").map(|i| i.to_owned()).collect()
+}
+
 #[cfg(test)]
-mod test {
+mod parser_test {
     use super::pad_input;
-    
+    use super::tokenize;
+
     #[test]
     fn pad_input_should_insert_spaces_before_and_after_parens() {
-        assert!(pad_input(~"(x 1 2 3)") == ~" ( x 1 2 3 ) ");
+        assert_eq!(pad_input(~"(x 1 2 3)"), ~" ( x 1 2 3 ) ");
+    }
+
+    #[test]
+    fn tokenize_should_split_string_into_tokens() {
+        let tokens = tokenize(~"(foo 12 3)");
+        assert_eq!(tokens.len(), 5);
     }
 }
