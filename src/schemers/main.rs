400d69248c5052af90d2b066b08669bed9acf3ff
Jeff Olson <olson.jeffery@gmail.com>
2014-05-16 23:45:19 -0500

    integrating read() into main() function

--- src/schemers/main.rs
+++ src/schemers/main.rs
@@ -20,7 +20,8 @@ fn main() {
     }
     let program: ~str = args.move_iter()
         .fold(~"", |memo, arg| memo + arg + " ").trim().to_owned();
-    println!("{}", tokenize(program));
+    let tokens = &mut tokenize(program);
+    println!("{:?}", read(tokens));
 }
 
 // parser impl
@@ -39,9 +40,7 @@ pub enum ParseItem {
 }
 
 fn read(tokens: &mut Vec<~str>) -> ParseItem {
-    if tokens.len() == 0 {
-        fail!("calling read() with an empty tokens vector");
-    }
+    println!("current state of tokens: '{:?}'", tokens);
     match tokens.pop().expect("calling read() w/ empty token list; shouldn't happen") {
         ref x if *x == ~"(" => {
             let mut list = Vec::new();
@@ -66,16 +65,25 @@ mod parser_test {
     }
 
     #[test]
-    fn tokenize_should_split_string_into_tokens() {
+    fn tokenize_string_into_tokens() {
         let tokens = tokenize(~"(foo 12 3)");
         assert_eq!(tokens.len(), 5);
+        assert_eq!(tokens.get(0).to_owned(), ~"(");
+        assert_eq!(tokens.get(1).to_owned(), ~"foo");
+        assert_eq!(tokens.get(2).to_owned(), ~"12");
+        assert_eq!(tokens.get(3).to_owned(), ~"3");
+        assert_eq!(tokens.get(4).to_owned(), ~")");
     }
 
     #[test]
     fn parse_should_take_a_vector_consisting_an_atom_and_convert_it_to_a_parse_item() {
         let tokens = &mut tokenize(~"bar");
-        match read(tokens) {
-            Atom(val) => { assert_eq!(val, ~"bar")},
+        let parsed_item = read(tokens);
+        println!("{:?}", parsed_item);
+        match parsed_item {
+            Atom(val) => {
+                assert_eq!(val, ~"bar")
+            },
             _ => assert!(false)
         }
     }
