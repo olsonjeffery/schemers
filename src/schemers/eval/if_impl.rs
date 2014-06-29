fd75c31895d6b072f883d73607019c57c1748ba7
Jeff Olson <olson.jeffery@gmail.com>
2014-06-29 07:19:49 -0500

    fix license

--- src/schemers/eval/if_impl.rs
+++ src/schemers/eval/if_impl.rs
@@ -1,3 +1,10 @@
+// Copyright 2014 Jeffery Olson
+//
+// Licensed under the 3-Clause BSD License, see LICENSE.txt
+// at the top-level of this repository.
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use super::{EvalResult, eval};
 use expr::{Expr, Atom, Boolean, List};
 use env::Env;
